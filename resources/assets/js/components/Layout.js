da05bb8fd52ef2fd55ea7111b1fdf827cb31a972
Alexander Letnikow <reclamme@gmail.com>
2017-06-03 20:26:14 +0300

    Change frontend configuration

---
+++ resources/assets/js/components/Layout.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+const Layout = class Layout extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>Hello, world!</h1>;
+    }
+};
+
+export default Layout; 
